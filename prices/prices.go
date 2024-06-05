6ebe4dd4ca1571d9de53e789263e86ce238f7198
fon <anatsaya.lee@gmail.com>
2024-06-05 21:55:49 +0700

    Create prices package to calculate InputPrices of each TaxRate

---
+++ prices/prices.go
@@ -0,0 +1,26 @@
+package prices
+
+import "fmt"
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, inputPrice := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", inputPrice)] = inputPrice * (1 + job.TaxRate)
+	}
+	fmt.Print(result)
+
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
